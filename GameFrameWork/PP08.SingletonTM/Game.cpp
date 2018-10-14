f838d2d4f82008ed4049655655f2ce1cb119fd83
정희 한 <jhhan803@outlook.kr>
2018-10-15 00:33:05 +0900

    PP08.Singleton 10월14일

--- GameFrameWork/PP08.SingletonTM/Game.cpp
+++ GameFrameWork/PP08.SingletonTM/Game.cpp
@@ -6,48 +6,47 @@
 
 float speed = 1.0f;
 
-bool Game::init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen)
+bool Game::init(const char* title, int xpos, int ypos,
+	int width, int height, bool fullscreen)
 {
 	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
 	{
-		m_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height,
-			SDL_WINDOW_SHOWN);
+		m_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, SDL_WINDOW_SHOWN);
 		if (m_pWindow != 0)
 		{
 			m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
 		}
 
 		m_bRunning = true;
-		//	SDL_Surface*pTempSurface = SDL_LoadBMP("assets/animate.bmp");
-		//	SDL_Surface*pTempSurface = IMG_Load("assets/animate-alpha.png");
-		m_textureManager.load("assets/animate-alpha.png", "animate", m_pRenderer);
-		/*m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
-		SDL_FreeSurface(pTempSurface);
-		SDL_QueryTexture(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);
-		m_sourceRectangle.w = 128;
-		m_sourceRectangle.h = 82;
-		m_destinationRectangle.x = m_sourceRectangle.x = 0;
-		m_destinationRectangle.y = m_sourceRectangle.y = 0;
-		m_destinationRectangle.w = 128;
-		m_destinationRectangle.h = 82;
-		SDL_SetRenderDrawColor(m_pRenderer, 255, 0, 0, 255);*/
+
+		// load 부분 대치   
+		if (!TheTextureManager::Instance()->load("assets/animate-alpha.png", "animate", m_pRenderer))
+		{
+			return false;
+		}
+
+		
+
 	}
 	else {
 		return false;
 	}
-
-
 	return true;
 }
-
 void Game::render()
 {
-
 	SDL_RenderClear(m_pRenderer);
-	m_textureManager.draw("animate", 0, 0, 128, 82, m_pRenderer);
-	m_textureManager.drawFrame("animate", 100, 100, 128, 82, 1, m_currentFrame, m_pRenderer);
-	//SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
+
+	
+	TheTextureManager::Instance()->draw("animate", 0, 0, 128, 82,
+		m_pRenderer);
+
+	TheTextureManager::Instance()->drawFrame("animate", 100, 100,
+		128, 82, 1, m_currentFrame, m_pRenderer);
+
+	
 	SDL_RenderPresent(m_pRenderer);
+
 }
 void Game::update()
 {
@@ -98,4 +97,4 @@ void Game::handleEvents()
 		}
 	}
 }
-#pragma once
+
--- GameFrameWork/PP08.SingletonTM/TextureManager.h
+++ GameFrameWork/PP08.SingletonTM/TextureManager.h
@@ -3,14 +3,23 @@
 #include <map>
 #include "SDL.h"
 
-class TextureManager
-{
+class TextureManager {
+private:
+	 static TextureManager* s_pInstance;
+    TextureManager() {}
+
 public:
-	TextureManager() {}
 	~TextureManager() {}
-	bool load(std::string fileName, std::string id, SDL_Renderer*pRenderer);
-	void draw(std::string id, int x, int y, int width, int hdeight, SDL_Renderer* pRenderer, SDL_RendererFlip flip = SDL_FLIP_NONE);
-	void drawFrame(std::string id, int x, int y, int width, int height, int currentRow, int currentFrame, SDL_Renderer*pRenderer, SDL_RendererFlip flip = SDL_FLIP_NONE);
+
+	bool load(std::string fileName, std::string id, SDL_Renderer* pRenderer);
+
+	void draw(std::string id, int x, int y, int width, int height,
+		SDL_Renderer* pRenderer, SDL_RendererFlip flip = SDL_FLIP_NONE);
+
+	void drawFrame(std::string id, int x, int y, int width, int height,
+		int currentRow, int currentFrame,
+		SDL_Renderer* pRenderer, SDL_RendererFlip flip = SDL_FLIP_NONE);
+
 	static TextureManager* Instance()
 	{
 		if (s_pInstance == 0)
@@ -20,8 +29,10 @@ public:
 		}
 		return s_pInstance;
 	}
+
 private:
-	//std::map<std::string, SDL_Texture*>m_textureMap;
-	TextureManager() {}
-	static TextureManager* s_pInstance;
+
+	std::map<std::string, SDL_Texture*> m_textureMap;
 };
+
+typedef TextureManager TheTextureManager;
