5c7a62147ee6fdd972e0b422a2b0300d87400c8c
user <user@532-15>
2018-12-04 13:23:35 +0900

    뭐가문제지

--- GameFrameWork/MiniGame/PlayState.cpp
+++ GameFrameWork/MiniGame/PlayState.cpp
@@ -12,23 +12,24 @@ const std::string PlayState::s_playID = "PLAY";
 PlayState* PlayState::s_pInstance = 0;
 void PlayState::update()
 {
-	if (TheInputHandler::Instance()->isKeyDown(
-		SDL_SCANCODE_ESCAPE))
+	if (checkCollision(
+		dynamic_cast<SDLGameObject*>(m_gameObjects[0]), dynamic_cast<SDLGameObject*>(m_gameObjects[1])))
 	{
 		TheGame::Instance()->getStateMachine()->changeState(
-			new	PauseState());
+			new GameOverState());
 	}
 	for (int i = 0; i < m_gameObjects.size(); i++)
 	{
 		m_gameObjects[i]->update();
 	}
-	if (checkCollision(
-		dynamic_cast<SDLGameObject*>(m_gameObjects[0]),
-		dynamic_cast<SDLGameObject*>(m_gameObjects[1])))
+
+	
+    if (TheInputHandler::Instance()->isKeyDown(
+		SDL_SCANCODE_ESCAPE))
 	{
-		TheGame::Instance()->getStateMachine()->changeState(
-			new GameOverState());
+		TheGame::Instance()->getStateMachine()->changeState(new	PauseState());
 	}
+	
 }
 void PlayState::render()
 {
